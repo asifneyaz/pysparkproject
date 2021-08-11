3c2eed91e73c280a6cd7912f280c0a1b8c05dd1b
[asifneyaz] <asifneyaz@gmail.com>
2021-08-11 08:41:38 -0500

    initial

---
+++ flightdata/flight.py
@@ -0,0 +1,7 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("flight").master("local[*]").getOrCreate()
+flightdata = spark.read.option("inferSchema", True).option("header", True).csv("2015-summary.csv")
+flightdata.createOrReplaceTempView("flight_data_view")
+sqlway = spark.sql("""SELECT DEST_COUNTRY_NAME, COUNT(1) FROM flight_data_view GROUP BY DEST_COUNTRY_NAME """)
+sqlway.show(10)
+dataframeway = flightdata.groupby('DEST_COUNTRY_NAME').sum("count").withColumnRenamed("sum(count)", "destination_total").sort("destination_total", ascending=False).limit(5).show()
